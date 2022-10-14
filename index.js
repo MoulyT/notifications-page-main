60d9599a348e88958a6fc0bb3e1077952abd944e
MoulyT <109950811+MoulyT@users.noreply.github.com>
2022-10-14 10:15:28 +0200

    Added documentation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// The objective of the script is to handle notifications. It should allow
+// to count de number of notification with counter(), to mark all as read
+// with markAllAsReaded() and to change the style of the notification
+// when you click in the card with markAsReaded()
+
 const cardUnreadNodeList = document.querySelectorAll(".card--unread");
 const cardUnread = Array.from(cardUnreadNodeList);
 const markAllAsReadButton = document.getElementById("button__all_read");
@@ -9,6 +14,9 @@ let NumberOfNotifications = 0;
 
 counter();
 
+// counter() not only counts the notifications but it resets the counter every time
+// its invoked. It reade how many class "card--unread" there are in the DOM and
+// displays it in the HTML class.
 function counter() {
   let NumberOfNotifications = 0;
   for (let i = 0; i < cardUnread.length; i++) {
@@ -29,6 +37,10 @@ cardUnread.forEach(function (card) {
   });
 });
 
+// the attribute of markAsReaded(event) is the element that's clicked
+// If it contains the "card--unread" class it removes it from the
+// card and removes the unread dot style
+
 function markAsReaded(event) {
   let card = event.target;
   if (card.classList.contains("card--unread")) {
@@ -39,6 +51,8 @@ function markAsReaded(event) {
   }
   counter();
 }
+
+//markAllAsReaded() does what is name inficates.
 
 function markAllAsReaded() {
   for (let i = 0; i < cardUnread.length; i++) {
